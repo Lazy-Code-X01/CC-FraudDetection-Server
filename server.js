24aeaa5804842d5a233269954ad938348b4245eb
Lazy-Code-X01 <lazycode686@gmail.com@>
2024-07-18 01:43:26 +0100

    version 1.1

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/userMiddleware.js";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
+
+import cors from 'cors'
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -16,6 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+
+app.use(cors())
 
 app.use("/api/users", userRoutes);
 
